3f13f876551c109e8ddc9dd822f0610caf6a3143
Wang Yalan <smilelan0502@gmail.com>
2020-06-03 19:06:43 +0900

    项目初始化

---
+++ idea-git-test1/src/lesson/Test01.java
@@ -0,0 +1,17 @@
+package lesson;
+
+/**
+ * @author tom
+ * @version V1.0
+ * @Package lesson
+ * @date 2020/6/3 18:52
+ * @Copyright ©
+ */
+public class Test01 {
+    public static void main(String[] args) {
+        System.out.println("测试");
+    }
+
+
+
+}
---
+++ idea-git-test1/src/lesson/Test2.java
@@ -0,0 +1,18 @@
+package lesson;
+
+/**
+ * @author tom
+ * @version V1.0
+ * @Package lesson
+ * @date 2020/6/3 19:02
+ * @Copyright ©
+ */
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("测试");
+        int ZX = 1 ;
+
+        System.out.println(ZX);
+    }
+
+}
